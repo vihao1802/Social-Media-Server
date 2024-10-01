7c113e6fd080daa444f8a584ddfef0a17bd14b43
tikjuti <tangquoctuan2003@gmail.com>
2024-10-01 22:06:43 +0700

    add exceptions and services folder

---
+++ Exceptions/AppError.cs
@@ -0,0 +1,12 @@
+﻿namespace SocialMediaServer.Exceptions
+{
+    public class AppError: Exception
+    {
+        public int StatusCode { get; }
+
+        public AppError(string message, int statusCode = 400) : base(message)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
