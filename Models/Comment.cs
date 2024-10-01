0b4d190e690634c676a7dbaaa23a20170774cb65
hnoga-n <hoanghuynguyen711@gmail.com>
2024-10-01 14:44:42 +0700

    feat [Data]: add Dbcontext

---
+++ Models/Comment.cs
@@ -0,0 +1,18 @@
+namespace SocialMediaServer.Models;
+public class Comment
+{
+    public int Id { get; set; }
+    public string Content { get; set; } = string.Empty;
+    public string Content_gif { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public int UserId { get; set; }
+    public User User { get; set; }
+    public int PostId { get; set; }
+    public Post Post { get; set; }
+
+    public int? CommentId { get; set; }
+    public Comment? ParentComment { get; set; }
+
+    public List<Comment> Replies { get; set; } = new List<Comment>();
+    public List<CommentReaction> CommentReactions { get; set; } = new List<CommentReaction>();
+}
---
+++ Models/MessengeMediaContent.cs
@@ -0,0 +1,9 @@
+namespace SocialMediaServer.Models;
+public class MessengeMediaContent
+{
+    public int Id { get; set; }
+    public string Media_url { get; set; } = string.Empty;
+    public string Media_type { get; set; } = string.Empty;
+    public int MessengeId { get; set; }
+    public Messenge Messenge { get; set; }
+}
