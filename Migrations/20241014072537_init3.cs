985781440414caf13b5127a0fa96a49d83a66bcc
hnoga-n <hoanghuynguyen711@gmail.com>
2024-10-14 14:28:18 +0700

    fix: fix security

---
+++ Migrations/20241014072537_init3.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace SocialMediaServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class init3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "0c83d767-5c0d-49bd-9c4b-77ef50659bd7");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "39875189-8e19-45b4-a52a-5372721e963d");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "ce91c250-c5a9-48f9-a194-784752768626", null, "User", "USER" },
+                    { "f5b52c54-7eb3-482e-a4fa-2194576257e5", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ce91c250-c5a9-48f9-a194-784752768626");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f5b52c54-7eb3-482e-a4fa-2194576257e5");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "0c83d767-5c0d-49bd-9c4b-77ef50659bd7", null, "User", "USER" },
+                    { "39875189-8e19-45b4-a52a-5372721e963d", null, "Admin", "ADMIN" }
+                });
+        }
+    }
+}
