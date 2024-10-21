be89e6c9092c37fde64213719d1a14a132904069
tikjuti <tangquoctuan2003@gmail.com>
2024-10-21 16:50:50 +0700

    add Post's api

---
+++ Repositories/Implementations/PostRepository.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialMediaServer.Data;
+using SocialMediaServer.Models;
+using SocialMediaServer.Repositories.Interfaces;
+
+namespace SocialMediaServer.Repositories.Implementations
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly ApplicationDBContext _dbContext;
+
+        public PostRepository(ApplicationDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<List<Post>> GetAllPostsAsync()
+        {
+            var posts = _dbContext.Posts
+                .Where(post => post.Visibility == Visibility.Public)
+                .ToListAsync();
+            return posts;
+        }
+
+        public Task<List<Post>> GetAllPostsPublicByUserIdAsync(string userId)
+        {
+            var posts = _dbContext.Posts
+                .Where(post => post.CreatorId == userId && post.Visibility == Visibility.Public)
+                .ToListAsync();
+            return posts;
+        }
+
+        public Task<List<Post>> GetAllPostsOnlyFriendByUserIdAsync(string userId)
+        {
+            var posts = _dbContext.Posts
+                .Where(post => post.CreatorId == userId && (post.Visibility == Visibility.Public || post.Visibility == Visibility.FriendsOnly))
+                .ToListAsync();
+            return posts;
+        }
+
+        public Task<List<Post>> GetAllPostsByMeAsync(string meId)
+        {
+            var posts = _dbContext.Posts
+                .Where(post => post.CreatorId == meId)
+                .ToListAsync();
+            return posts;
+        }
+
+        public async Task<Post> GetByIdAsync(int id)
+        {
+            return await _dbContext.Posts.FirstOrDefaultAsync(p => p.Id == id) ?? throw new ArgumentNullException(nameof(id));
+        }
+
+        public async Task<Post> CreateAsync(Post post)
+        {
+            _dbContext.Posts.Add(post);
+            await _dbContext.SaveChangesAsync();
+            return post;
+        }
+
+        public async Task<Post> UpdateAsync(Post post)
+        {
+            _dbContext.Posts.Update(post);
+            await _dbContext.SaveChangesAsync();
+            return post;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var post = await _dbContext.Posts.FirstOrDefaultAsync(p => p.Id == id);
+            if (post == null)
+            {
+                return false;
+            }
+            _dbContext.Posts.Remove(post);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+    }
+}
---
+++ Services/Interfaces/IPostService.cs
@@ -0,0 +1,17 @@
+﻿using DTOs.Response;
+using SocialMediaServer.DTOs.Request.Post;
+
+namespace SocialMediaServer.Services.Interfaces
+{
+    public interface IPostService
+    {
+        Task<List<PostResponseDTO>> GetAllAsync();
+        Task<List<PostResponseDTO>> GetAllByUserIdAsync(string userViewId);
+        Task<List<PostResponseDTO>> GetAllByMeAsync();
+        Task<PostResponseDTO> GetByIdAsync(int id);
+        Task<PostResponseDTO> CreateAsync(PostCreateDTO postCreateDTO);
+        Task<PostResponseDTO> UpdateAsync(PostUpdateDTO post, int id);
+        Task<PostResponseDTO> PatchAsync(PostPatchDTO post, int id);
+        Task<bool> DeleteAsync(int id);
+    }
+}
