cba077eb0a71a36ac5fe568df046f1e83f9fbf39
vihao <tranvihao40@gmail.com>
2024-12-04 19:41:35 +0700

    fix: swagger production not found

--- Program.cs
+++ Program.cs
@@ -271,18 +271,8 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-} else {
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-        c.RoutePrefix = string.Empty; // Serve the Swagger UI at the app's root
-    });
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
